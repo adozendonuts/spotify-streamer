0098de0df9824fc9e18d7a1038b60fe9b55f3186
dean <isoybean@gmail.com>
2015-08-20 20:24:58 -0400

    added functionality to all buttons. next/previous. updateViews helper

--- app/src/main/java/com/runningoutofbreadth/spotifystreamer/TrackPlayerFragment.java
+++ app/src/main/java/com/runningoutofbreadth/spotifystreamer/TrackPlayerFragment.java
@@ -5,7 +5,6 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
